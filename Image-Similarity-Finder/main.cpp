a351906cc062e6dc379dbdad290f5edf105b62b7
Lucas Alves Sobrinho <lucas@lucass-macbook-pro.local>
2016-09-23 21:15:29 -0300

    Initial Commit

---
+++ Image-Similarity-Finder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Image-Similarity-Finder
+//
+//  Created by Lucas Alves Sobrinho on 9/23/16.
+//  Copyright © 2016 Lucas Alves Sobrinho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
